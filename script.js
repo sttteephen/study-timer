a4ecabdceb364c1a7923c3846e212ed514274359
Stephen Messer <40314216@live.napier.ac.uk>
2022-06-20 12:33:53 +0100

    name timer

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function CountDown() {
         //console.log("start clock")
         document.getElementById("progress-bar").classList.replace("invisible", "visible");
         this.counting = true;
+        this.showTaskName();
     }
 
     this.pauseClock = function () {
@@ -33,11 +34,11 @@ function CountDown() {
         this.progress = 1;
         document.getElementById("progress-bar").classList.replace("visible", "invisible");
         this.updateScreen();
+        this.showTaskInputs();
     }
 
     this.changeDuration = function (e) {
-        // try using regex here
-        duration = parseInt(e.innerHTML.slice(0, 2));
+        duration = parseInt(e.innerHTML.match(/[0-9][0-9]/g)[0]);
         this.initialSeconds = duration * 60
         this.currentSeconds = this.initialSeconds;
         this.updateScreen();
@@ -56,6 +57,27 @@ function CountDown() {
         document.getElementById("minutes").innerHTML = minutes.padStart(2, "0");
         document.getElementById("seconds").innerHTML = seconds.padStart(2, "0");
         document.getElementById("complete-progress").setAttribute("style", `width: ${String(this.progress)}%`);
+    }
+
+    this.showTaskName = function () {
+        nameInput = document.getElementById("task-name-input");
+        inputDiv = document.getElementById("name-duration");
+        nameDiv = document.getElementById("task-name-div");
+        nameH3 = document.getElementById("task-name-h3");
+
+        newName = nameInput.value;
+
+        if (newName) {
+            nameH3.innerHTML = newName;
+            nameDiv.classList.replace("invisible", "visible");
+        }
+
+        inputDiv.classList.replace("visible", "invisible");
+    }
+
+    this.showTaskInputs = function () {
+        document.getElementById("task-name-div").classList.replace("visible", "invisible");
+        document.getElementById("name-duration").classList.replace("invisible", "visible");
     }
 }
 
