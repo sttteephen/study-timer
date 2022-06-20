f58f039543fc70656e24524b59759c5035bbab48
Stephen Messer <40314216@live.napier.ac.uk>
2022-06-20 14:55:03 +0100

    added comments

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ function CountDown() {
     this.currentSeconds = this.initialSeconds;
     this.progress = 1;
 
+    /*
+    when counting, decrements the current seconds value and updates the progress percentage then calls update screen to show changes
+    */
     this.decrementClock = function () {
         //console.log("decrement clock " + this.counting)
         if (this.counting && this.currentSeconds > 0) {
             this.currentSeconds--;
             this.progress = Math.ceil(((this.initialSeconds - this.currentSeconds) / this.initialSeconds) * 100);
-            console.log(this.progress);
+            //console.log(this.progress);
             this.updateScreen();
         }
     }
 
+    // start the clock decrementing and show progress bar
     this.startClock = function () {
         //console.log("start clock")
         document.getElementById("progress-bar").classList.replace("invisible", "visible");
@@ -22,11 +26,13 @@ function CountDown() {
         this.showTaskName();
     }
 
+    // stop the clock decrementing
     this.pauseClock = function () {
         //console.log("pause clock")
         this.counting = false;
     }
 
+    // set timer back to the initial duration
     this.resetClock = function () {
         //console.log("reset clock")
         this.counting = false;
@@ -37,6 +43,7 @@ function CountDown() {
         this.showTaskInputs();
     }
 
+    // change the length of the timer when a new duration is choosen
     this.changeDuration = function (e) {
         duration = parseInt(e.innerHTML.match(/[0-9][0-9]/g)[0]);
         this.initialSeconds = duration * 60
@@ -44,6 +51,7 @@ function CountDown() {
         this.updateScreen();
     }
 
+    // convert the timers seconds value to minutes and seconds and return as string
     this.secToMinStr = function () {
         let mins = Math.floor(this.currentSeconds / 60);
         let secs = this.currentSeconds % 60;
@@ -51,6 +59,7 @@ function CountDown() {
         return { minutes: String(mins), seconds: String(secs) }
     }
 
+    // update the minutes and seconds values and progress bar on screen
     this.updateScreen = function () {
         //console.log("updating screen")
         let { minutes, seconds } = this.secToMinStr();
@@ -59,27 +68,28 @@ function CountDown() {
         document.getElementById("complete-progress").setAttribute("style", `width: ${String(this.progress)}%`);
     }
 
+    /* 
+    display the current value of the text input as the task name and 
+    remove the text input and duration button from the screen
+    */
     this.showTaskName = function () {
-        nameInput = document.getElementById("task-name-input");
-        inputDiv = document.getElementById("name-duration");
-        nameDiv = document.getElementById("task-name-div");
-        nameH2 = document.getElementById("task-name-h2");
-
-        newName = nameInput.value;
+        const newName = document.getElementById("task-name-input").value;
 
         if (newName) {
-            nameH2.innerHTML = newName;
-            nameDiv.classList.replace("invisible", "visible");
+            document.getElementById("task-name-h2").innerHTML = newName;
+            document.getElementById("task-name-div").classList.replace("invisible", "visible");
         }
 
-        inputDiv.classList.replace("visible", "invisible");
+        document.getElementById("name-duration").classList.replace("visible", "invisible");
     }
 
+    // remove the task name from the screen and show the task input and duration button
     this.showTaskInputs = function () {
         document.getElementById("task-name-div").classList.replace("visible", "invisible");
         document.getElementById("name-duration").classList.replace("invisible", "visible");
     }
 }
+
 
 const counter = new CountDown();
 counter.updateScreen()
